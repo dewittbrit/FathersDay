e01bba7797ac46b220fd909e28a885f11e57e616
dewittbrit <szabobrit@gmail.com>
2024-05-07 19:25:32 -0400

    SetUp

--- FathersDay/src/App.jsx
+++ FathersDay/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import SignUp from '../components/SignUp'
 import './App.css'
 
 function App() {
@@ -8,6 +9,11 @@ function App() {
 
   return (
     <>
+
+
+    {/* <Routes>
+      <Route path="</Route>
+    </Routes> */}
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
