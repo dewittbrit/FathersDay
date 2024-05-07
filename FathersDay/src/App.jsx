9275aef1c163c341b89192bf9ba1eee7273db9d0
dewittbrit <szabobrit@gmail.com>
2024-05-07 19:38:58 -0400

    MainPageUpdate

--- FathersDay/src/App.jsx
+++ FathersDay/src/App.jsx
@@ -23,7 +23,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Welcome to Chef Couch's Kitchen!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
